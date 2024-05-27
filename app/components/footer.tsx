5a4e5f338094034cc5f5f1394ea9da592536725b
ultratrikx <rohanthm@outlook.com>
2024-05-27 18:19:23 -0400

    chore: Update social media links in footer component

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -53,20 +53,17 @@ export default function Footer() {
           </a>
         </li>
         <li>
-        <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="www.linkedin.com/in/rohanth-marem"
+            href="https://www.linkedin.com/in/rohanth-marem"
           >
             <ArrowIcon />
             <p className="ml-2 h-7">linkedin</p>
           </a>
         </li>
         <li>
-
-        </li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
@@ -77,14 +74,14 @@ export default function Footer() {
             <p className="ml-2 h-7">view source</p>
           </a>
         </li>
-      </ul>
-      <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
-      </p>
-    {/* <form id="newsletter-form">
-      <input type="email" id="email" placeholder="Your email" required></input>
-      <button type="submit">Subscribe</button>
-    </form> */}
-    </footer>
-  )
+        </ul>
+        <p className="mt-8 text-neutral-600 dark:text-neutral-300">
+          © {new Date().getFullYear()} MIT Licensed
+        </p>
+        {/* <form id="newsletter-form">
+          <input type="email" id="email" placeholder="Your email" required></input>
+          <button type="submit">Subscribe</button>
+        </form> */}
+        </footer>
+)
 }
