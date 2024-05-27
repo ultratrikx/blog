ac28323fac98f10b3159802505a96f4127898367
ultratrikx <rohanthm@outlook.com>
2024-05-27 18:25:10 -0400

    chore: Update page titles and content in blog and landing pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        rohanth's notes
+        rohanth marem
       </h1>
       <p className="mb-4">
-        {`I'm Rohanth, a grade 10 student based in Toronto, Canada. I'm passionate about technology and I love to build things.`}
+        {`I'm Rohanth, a grade 10 student based in Toronto. 
+        This is my public notebook, where I write about things I'm learning, projects I'm working on and other things I find interesting.
+        Feel free to reach out to me at  `}<a href="mailto:rohanthm@outlook.com">rohanthm@outlook.com.</a>
       </p>
       <div className="my-8">
         <BlogPosts />
